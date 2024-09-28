f1ffb7a90390803a626b523acd9e5ad1adfbd05e
V0D14Ka <minecraft35510880@gmail.com>
2024-09-28 07:45:25 +0000

    update bot

--- bot/handlers/general.py
+++ bot/handlers/general.py
@@ -7,6 +7,12 @@ from create_bot import bot
 async def echo(message: types.Message, **kwargs):
     await bot.send_message(message.from_user.id, message.text)
 
+async def api_predict(message: types.message, **kwargs):
+    data_json = {"question": f"{message.text}"}
+    resp = requests.post("http://87.242.119.60:85/predict", json=data_json, timeout=(5, 15))
+    answer_json = resp.json()
+    
+    await message.reply(f"{answer_json['answer']}")
 
 def register_handlers_general(_dp: Dispatcher):
-    _dp.register_message_handler(echo)
+    _dp.register_message_handler(api_predict)
