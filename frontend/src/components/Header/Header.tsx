d5ddcc39c3941978bd6765b820cb1915bc99d3fb
Daniil <c.cffc@mail.ru>
2024-09-28 12:39:17 +0300

    Update logo

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import styles from './Header.module.css';
 const Header: FC = () => {
   return (
     <div className={styles.header}>
-      <img src="/public/Logo_RUTUBE_dark_color.png" alt="Logo" width="150" />
+      <img src="/Logo_RUTUBE_dark_color.png" alt="Logo" width="150" />
     </div>
   );
 };
