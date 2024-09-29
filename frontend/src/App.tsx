0beafa7796ade0897915a43328ddefa7ffe66ee4
Daniil <c.cffc@mail.ru>
2024-09-29 04:36:42 +0300

    Fix bug

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -33,6 +33,18 @@ const App: FC = () => {
         />
         <ChatBox onBotReply={addMessage} onBotEdit={addInputValue} />
       </div>
+      {/* <div>
+        <Header />
+        <div className={styles.container}>
+          <Chat
+            messages={messages}
+            addMessage={addMessage}
+            inputValue={inputValue}
+            addInputValue={addInputValue}
+          />
+          <ChatBox onBotReply={addMessage} onBotEdit={addInputValue} />
+        </div>
+      </div> */}
     </div>
   );
 };
