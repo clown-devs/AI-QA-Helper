b05cbf558ef0ea5e3dafac8d9427b2d0baae3e63
Simoffsky <maks-kirsanov26@mail.ru>
2024-09-29 08:41:12 +0300

    remove useless scripts

--- db/sample_usage.py
+++ db/sample_usage.py
@@ -2,6 +2,8 @@ import chromadb
 from chromadb import Documents, EmbeddingFunction, Embeddings
 from embedding import HFEmbeddingFunction
 
+# Скрипт демонстрирует пример использования базы данных ChromaDB
+
 chroma_client = chromadb.HttpClient(host='87.242.119.60', port=8000)
 
 emedding_func = HFEmbeddingFunction()
@@ -9,12 +11,10 @@ collection = chroma_client.get_collection(
     name="rutube", 
     embedding_function=emedding_func
 )
-import time
-time_start = time.time()
+
 results = collection.query(
     query_texts=["Могу ли я загружать видео в формате MP5"], 
     n_results=3 # сколько результатов вернуть
 )
-time_end = time.time()
-print("MEASURE:",time_end - time_start)
+
 print(results)
